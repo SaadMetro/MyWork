23818104f7a776422748b1b06637fe258683a40a
mujtaba ali <mujtabakalmati@gmail.com>
2022-12-15 16:31:04 +0500

    This is my first commit

---
+++ JS/fruit data.js
@@ -0,0 +1,25 @@
+var products = [ 
+    {"Id":0 , "Name":"","Brand":"","Price":"","Description":"","Image":""} ,
+
+    {"Id":1 , "Name":"Mixed Fruit Cake","Brand":"Cake Shop","Price":"1000.00","Description":"Mixed Friut Cake with fresh cream and three fruit mango,strawberry and pineapple.","Image":"image/f1.jpg"} ,
+    
+    {"Id":2 , "Name":"","Brand":"","Price":"63000","Description":"	","Image":""} ,
+    
+    {"Id":3 , "Name":"","Brand":"","Price":"57000","Description":" ","Image":""} ,
+    
+    
+    {"Id":4 , "Name":"","Brand":"","Price":"58000","Description":" ","Image":""} ,
+    
+    {"Id":5, "Name":"","Brand":"","Price":"57500","Description":"","Image":""} ,
+    
+    {"Id":6, "Name":"","Brand":"","Price":"30000","Description":"	","Image":""} ,
+    
+    {"Id":7, "Name":"","Brand":"","Price":"27500","Description":"	 ","Image":""} ,
+    
+    {"Id":8, "Name":"","Brand":"","Price":"One Kydney","Description":"	 ","Image":""} ,
+    
+    {"Id":9, "Name":"","Brand":"","Price":"52000","Description":"","Image":""} ,
+    
+    {"Id":10, "Name":"","Brand":"","Price":"16000","Description":"","Image":""}
+    
+    ]
